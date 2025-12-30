dbdcb3db182dfbc12c83efcc0817c9715443c8ab
km7872 <km7872@rit.edu>
2025-12-30 12:26:24 -0800

    google api key changes into extractor

--- app/extractor/extractor.py
+++ app/extractor/extractor.py
@@ -1,4 +1,5 @@
-from openai import OpenAI
+# from openai import OpenAI
+from google.genai import Client
 import json
 from dotenv import load_dotenv
 import os
@@ -6,7 +7,8 @@ import re
 
 load_dotenv()
 
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+# client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+client = Client(api_key=os.getenv("G_API_KEY"))
 def safe_json_parse(content: str) -> dict:
     content = re.sub(r"```(?:json)?", "", content)
     content = content.replace("```", "").strip()
@@ -34,17 +36,30 @@ def extract_fields(text: str) -> dict:
         \"\"\"
         """
 
-    response = client.chat.completions.create(
-        model="gpt-4o-mini",
-        messages=[
-            {"role": "user", "content": prompt}
-        ],
-        temperature=0
-    )
+    # response = client.chat.completions.create(
+    #     model="gpt-4o-mini",
+    #     messages=[
+    #         {"role": "user", "content": prompt}
+    #     ],
+    #     temperature=0
+    # )
 
-    content = response.choices[0].message.content
+    # content = response.choices[0].message.content
     # print(type(content))
 
     # return json.loads(content)
-    return safe_json_parse(content)
+    response = client.models.generate_content(
+    model="gemini-2.5-flash-lite",
+    contents=[{
+        "role": "user",
+        "parts": [{"text": prompt}]
+    }],
+    config={
+        "response_mime_type": "application/json",
+        "temperature": 0
+    }
+)
+    content = response.text
+    return content
+    # return safe_json_parse(content)
     # return {}
